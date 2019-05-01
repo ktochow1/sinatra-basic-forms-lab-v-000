38170c87bdd977020059caf8e7b054f6c8bc3ebf
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-01 14:34:59 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,13 +1,13 @@
 class Puppy 
 attr_reader :breed
 attr_accessor :months_old, :name
-  def initialize(name, breed, age)
+  def initialize(name, breed, months_old)
     @name = name
-    @age = age
+    @months_old = months_old
     @breed = breed
   end
   
-  def months_old
-    @age = age 
-  end 
+  # def months_old
+  #   @age = age 
+  # end 
 end
