dc403a8f636ca78f0a14908493387d16c5dea9d7
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-01 16:04:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
   
   post '/puppy' do 
     @new_puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    binding.pry
     erb :display_puppy
   end
 end
